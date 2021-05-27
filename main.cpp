940ebcdb914c35297ced183a330e305af6406fd2
pang <devforupwork@gmail.com>
2021-05-28 00:49:45 +0800

    add Convert YUV

--- main.cpp
+++ main.cpp
@@ -14,6 +14,17 @@ int main( int argc, char** argv )
 	inputImg = cv::imread("..\\testing images\\input.jpg");
 	int64 t = cv::getTickCount();
     // processing
+	cv::Mat yuv(inputImg.rows*3/2, inputImg.cols, CV_8UC1);
+	cv::cvtColor(inputImg, yuv, cv::COLOR_BGR2YUV_I420);
+
+	cv::Rect rt(0,0, inputImg.cols, inputImg.rows);
+	cv::Mat Y; Y = yuv(rt);
+	/*
+	cv::Mat outY = oneHDR(Y);
+	yuv(rt) = outY;
+	cv::cvtColor(yuv, outputImg, cv::COLOR_YUV2BGR_I420);
+	*/
+
 	outputImg = oneHDR(inputImg);
 	double time = (cv::getTickCount() - t) / cv::getTickFrequency() * 1000;
 	printf("time: %f ms\n", time / 10);
